f9827542f12fcaa807ef37c141733ab82c3255a2
AMU\Martin Zarate <guilleusa2001@hotmail.com>
2018-02-22 09:35:54 -0800

    Clean up comments

--- app.py
+++ app.py
@@ -6,25 +6,21 @@ map = folium.Map(location=[39.002,-97.503], zoom_start=4)
 
 # read data from file
 data = pandas.read_csv("parks.txt")
-# print(data.columns)
+
+# split columns into Python lists
 name = list(data["place"])
 lat = list(data["lat"])
 lon = list(data["lon"])
 
 # create a feature group
 fg = folium.FeatureGroup(name="markers")
-# add markers to the feature group
-# locations = [[35,-116],[34,-117],[36,-115]]
 
-# iterating trhu 2 list simultaneosly
+# iterating trhu 3 list simultaneously
 for lt, ln, nm in zip(lat, lon, name):
     fg.add_child(folium.Marker(location=[lt, ln], popup=nm, icon=folium.Icon(color="blue")))
 
-
-# fg.add_child( folium.Marker( location=[35,-116], popup="Im a marker on a map", icon=folium.Icon(color="green") ) )
-# fg.add_child( folium.Marker( location=[34,-117], popup="Im a marker on a map too", icon=folium.Icon(color="blue") ) )
 # add the feature group to the map
 map.add_child(fg)
 
-# save the map
+# save the map to an html file, if file exist, it will be overridden
 map.save("map1.html")
